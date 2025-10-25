78d75b731f6ea9f13c581e6e38c56a652f87a59f
Teerapong <teera04164@gmail.com>
2025-10-25 15:39:36 +0700

    add store persit user

--- src/store/user.store.js
+++ src/store/user.store.js
@@ -1,20 +1,28 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 import { meService } from '../services/auth.service'
 
-const useUserStore = create((set) => ({
-  user: null,
-  setUser: (user) => set({ user }),
-  fetchUser: async () => {
-    try {
-      const data = await meService()
-      set({ user: data.data })
-      return data
-    } catch (error) {
-      console.error('Error fetching user:', error)
-      return null
+const useUserStore = create(
+  persist(
+    (set) => ({
+      user: null,
+      setUser: (user) => set({ user }),
+      fetchUser: async () => {
+        try {
+          const data = await meService()
+          set({ user: data.data })
+          return data
+        } catch (error) {
+          console.error('Error fetching user:', error)
+          return null
+        }
+      },
+      clearUser: () => set({ user: null })
+    }),
+    {
+      name: 'user-storage',
     }
-  },
-  clearUser: () => set({ user: null })
-}))
+  )
+)
 
 export default useUserStore
